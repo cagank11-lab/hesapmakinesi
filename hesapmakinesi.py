681f288bb1b29f5f4b81528e24530c3780c0bd4a
cagan <cagank11@gmail.com>
2025-08-31 20:01:43 +0300

    first commit

---
+++ hesapmakinesi.py
@@ -0,0 +1,25 @@
+# Kullanıcıdan hangi işlemi yapmak istediğini al
+islem = input("Yapmak istediğiniz işlemi girin (+, -, *, /): ")
+
+# Kullanıcıdan iki sayı al
+ilksayi = float(input("İlk sayıyı giriniz: "))
+ikincisayi = float(input("İkinci sayıyı giriniz: "))
+
+# İşlemi kontrol et ve sonucu hesapla
+if islem == '+':
+    sonuc = ilksayi + ikincisayi
+    print(f"Sonuç: {sonuc}")
+elif islem == '-':
+    sonuc = ilksayi - ikincisayi
+    print(f"Sonuç: {sonuc}")
+elif islem == '*':
+    sonuc = ilksayi * ikincisayi
+    print(f"Sonuç: {sonuc}")
+elif islem == '/':
+    if ikincisayi == 0:
+        print("Sıfıra bölme hatası!")
+    else:
+        sonuc = ilksayi / ikincisayi
+        print(f"Sonuç: {sonuc}")
+else:
+    print("Geçersiz işlem girdiniz.")
